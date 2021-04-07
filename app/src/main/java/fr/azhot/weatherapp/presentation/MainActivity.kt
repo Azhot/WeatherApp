d34dff4e274c698cb0c254192d37fcda557cab4a
Azhot <francois.jouvelot@gmail.com>
2021-04-07 15:50:20 +0200

    Created BaseActivity.

--- app/src/main/java/fr/azhot/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/fr/azhot/weatherapp/presentation/MainActivity.kt
@@ -3,25 +3,17 @@ package fr.azhot.weatherapp.presentation
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import fr.azhot.weatherapp.R
 import fr.azhot.weatherapp.databinding.ActivityMainBinding
-import fr.azhot.weatherapp.presentation.ui.city_list.CityListViewModel
 
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-    // variables
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inflate(it) }) {
 
     // overridden functions
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initViewBinding()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -44,8 +36,4 @@ class MainActivity : AppCompatActivity() {
 
 
     // functions
-    private fun initViewBinding() {
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
 }
